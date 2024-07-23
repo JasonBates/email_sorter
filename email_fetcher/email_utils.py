5911f314e88ee105021eb50a830f2f7de6f9f03c
Jason Bates <jason@jasonbates.com>
2024-07-23 16:03:54 +0100

    fix Pyline issues

--- email_fetcher/email_utils.py
+++ email_fetcher/email_utils.py
@@ -10,25 +10,27 @@ Usage:
     email_test_print(messages, number=5, lines=5)
 """
 
+import email.parser
 from itertools import islice
 import email
 from html import unescape
 from email.header import decode_header
 from email.parser import BytesParser
 import chardet
+from typing import Union
 
-def decode_rfc2047(byte_like_subjectline):
+def decode_rfc2047(byte_like_subject_line):
     """
     Decode a Subject line encoded using RFC 2047.
 
     Args:
-        byte_like_subjectline (bytes): The byte-like encoded Subject line.
+        byte_like_subject_line (bytes): The byte-like encoded Subject line.
 
     Returns:
         str: The decoded Subject line.
     """
     # Decode the byte-like object to a string
-    encoded_text = byte_like_subjectline.decode('utf-8')
+    encoded_text = byte_like_subject_line.decode('utf-8')
     # Decode the encoded header
     decoded_parts = decode_header(encoded_text)
     # Initialize an empty list to hold decoded strings
@@ -56,7 +58,7 @@ def email_test_print(messages, number=10, lines=10):
     Args:
         messages (dict): A dictionary containing email messages, where the keys are message IDs
                          and the values are dictionaries containing message data.
-        number (int, optional): The maximum number of messages to print. Defaults to 10.
+        number (int, optional): The maximum number of messages to return. Defaults to 10.
         lines (int, optional): The number of lines from the message body to print. Defaults to 10.
 
     Raises:
@@ -72,10 +74,10 @@ def email_test_print(messages, number=10, lines=10):
         from_field = f"{envelope.sender[0]}" if envelope.sender else ''
         # decode tricky subject line with encoding of emojis and the like
         subject = decode_rfc2047(envelope.subject)
-        
+
         # get the body of the message and decode it from BODY.PEEK[]
         body_bytes = data[b'BODY[]']
-        message = BytesParser().parsebytes(body_bytes)
+        message= BytesParser().parsebytes(body_bytes)
         body_text = ''
 
         # Go through multipart email and pull out the text with HTML as a fallback
@@ -85,28 +87,28 @@ def email_test_print(messages, number=10, lines=10):
                 if part.get_content_type() == 'text/plain':
                     # print(f"Processing text/plain")
                     try:
-                        body_text = part.get_payload(decode=True).decode()
+                        body_text = part.get_payload(decode=True).decode() # type: ignore
                         # print("Found text/plain part, setting body_text")
                     except UnicodeDecodeError:
                         body_text = 'Unable to decode message body'
                         break
                 elif part.get_content_type() == 'text/html' and not body_text:
                     try:
-                        body_html = part.get_payload(decode=True).decode()
-                        body_text = unescape(email.parser.Parser().parsestr(body_html).get_payload())
+                        body_html = part.get_payload(decode=True).decode() # type: ignore[attr-defined]
+                        body_text = unescape(email.parser.Parser().parsestr(body_html).get_payload()) # type: ignore
                         # print("Found text/html part, setting body_text")
                     except UnicodeDecodeError:
                         body_text = 'Unable to decode message body'
                         break
         else:
             try:
-                body_text = message.get_payload(decode=True).decode()
+                body_text = message.get_payload(decode=True).decode() # type: ignore
                 # print('just text... ')
             except UnicodeDecodeError:
-                result = chardet.detect(message.get_payload(decode=True))
+                result = chardet.detect(message.get_payload(decode=True)) # type: ignore
                 if result['encoding'] is not None:
                     try:
-                        body_text = message.get_payload(decode=True).decode(result['encoding'])
+                        body_text = message.get_payload(decode=True).decode(result['encoding']) # type: ignore
                     except UnicodeDecodeError:
                         body_text = 'Unable to decode message body'
                 else:
@@ -114,10 +116,11 @@ def email_test_print(messages, number=10, lines=10):
 
         body_lines = body_text.split('\n')
         first_x_lines = '\n'.join(body_lines[:lines])
+        
+        # this is where a dictionary needs to be returned and the printing stuff happens in main.py
 
-        print("=======================================================")
         print("=======================================================")
         print(f"{msg_id} \nSubject: {subject} \nFrom: {from_field} \nDate: {date_received}")
-        print(f"First {lines} lines of text content:\n{first_x_lines}\n")
- 
-
+        print(f"First {lines} lines of text content:\n")
+        print("-------------------------------------------------------")
+        print(f" {first_x_lines} \n")
