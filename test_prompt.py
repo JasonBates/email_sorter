5e260cf58836f39bef2a34c36466e22cf69149e2
Jason Bates <jason@jasonbates.com>
2024-07-24 21:50:29 +0100

    remove some files

--- test_prompt.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = os.path.join(os.path.dirname(__file__), 'LLM', 'prompt.md')
-
-with open('LLM/prompt.md', 'r') as file:
-    # Read the content of the file into a string
-    content = file.read()
-    
-print(content)
